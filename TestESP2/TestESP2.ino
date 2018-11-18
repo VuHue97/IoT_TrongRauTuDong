47f4b1f7bfdc674b1e0a336bd7459700cc79805c
Vũ Thị Huế <hoangiabaogiahan@gmail.com>
2018-11-18 19:21:20 +0700

    Update TestESP2/TestESP2.ino

---
+++ TestESP2/TestESP2.ino
@@ -0,0 +1,146 @@
+//them thu vien
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+
+//dat loai cam bien su dung
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+//ten, mat khau wifi se ket noi
+const char* ssid = "KhongWifi";
+const char* password = "hue00111";
+
+// Web Server on port 80
+WiFiServer server(80);
+
+// chan du lieu D1
+#define DHTPin D1
+//khoi tao cam bien DHT22
+DHT dht(DHTPin, DHTTYPE);
+
+// bien tam thoi
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
+
+// ham cai dat chi chay 1 lan khi khoi dong
+void setup() {
+  //khoi tao man hinh serial
+  Serial.begin(115200);
+  delay(10);
+
+  dht.begin();
+  
+  // ket noi wifi
+  //Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  // in dau . khi chua connect duoc wifi
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // chay web server
+  server.begin();
+  Serial.println("Web server running. Waiting for the ESP IP...");
+  delay(10000);
+  
+  // in dia chi ip cua ESP trong mang lan ESP duoc ket noi
+  Serial.println(WiFi.localIP());
+}
+
+// ham lap
+void loop() {
+  // nghe client moi trong mang lan
+  WiFiClient client = server.available();
+  
+  if (client) {
+    //Serial.println("New client");
+    // bolean to locate when the http request ends
+    boolean blank_line = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        
+        if (c == '\n' && blank_line) {
+            // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+            float h = dht.readHumidity();
+            // Read temperature as Celsius (the default)
+            float t = dht.readTemperature();
+            // Read temperature as Fahrenheit (isFahrenheit = true)
+            float f = dht.readTemperature(true);
+            // Check if any reads failed and exit early (to try again).
+            if (isnan(h) || isnan(t) || isnan(f)) {
+              Serial.println("Failed to read from DHT sensor!");
+              strcpy(celsiusTemp,"Failed");
+              strcpy(fahrenheitTemp, "Failed");
+              strcpy(humidityTemp, "Failed");         
+            }
+//           else{
+//              // Computes temperature values in Celsius + Fahrenheit and Humidity
+//              float hic = dht.computeHeatIndex(t, h, false);       
+//              dtostrf(hic, 6, 2, celsiusTemp);             
+//              float hif = dht.computeHeatIndex(f, h);
+//              dtostrf(hif, 6, 2, fahrenheitTemp);         
+//              dtostrf(h, 6, 2, humidityTemp);
+//              // You can delete the following Serial.print's, it's just for debugging purposes
+//              Serial.print("Humidity: ");
+//              Serial.print(h);
+//              Serial.print(" %\t Temperature: ");
+//              Serial.print(t);
+//              Serial.print(" *C ");
+//              Serial.print(f);
+//              Serial.print(" *F\t Heat index: ");
+//              Serial.print(hic);
+//              Serial.print(" *C ");
+//              Serial.print(hif);
+//              Serial.print(" *F");
+//              Serial.print("Humidity: ");
+//              Serial.print(h);
+//              Serial.print(" %\t Temperature: ");
+//              Serial.print(t);
+//              Serial.print(" *C ");
+//              Serial.print(f);
+//              Serial.print(" *F\t Heat index: ");
+//              Serial.print(hic);
+//              Serial.print(" *C ");
+//              Serial.print(hif);
+//              Serial.println(" *F");
+//            }
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            // your actual web page that displays temperature and humidity
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html>");
+            client.println("<head></head><body><h1>ESP8266 - Temperature and Humidity</h1><h3>Temperature in Celsius: ");
+            client.println(t);
+            client.println("*C</h3><h3>Temperature in Fahrenheit: ");
+            client.println(f);
+            client.println("*F</h3><h3>Humidity: ");
+            client.println(h);
+            client.println("%</h3><h3>");
+            client.println("</body></html>");     
+            break;
+        }
+        if (c == '\n') {
+          // when starts reading a new line
+          blank_line = true;
+        }
+        else if (c != '\r') {
+          // when finds a character on the current line
+          blank_line = false;
+        }
+      }
+    }  
+    // closing the client connection
+    delay(1);
+    client.stop();
+    //Serial.println("Client disconnected.");
+  }
+}   
